f3903e49ec94dcf135a74a6b96895e0c1534a167
Nolan Clavereuil <nclavereuil@dombtsig.local>
2022-09-21 13:35:50 +0200

    ajout du css

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
                 
                 <label><b>Nom d'utilisateur</b></label>
                 <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+                
+                <br>
 
                 <label><b>Mot de passe</b></label>
                 <input type="password" placeholder="Entrer le mot de passe" name="password" required>
