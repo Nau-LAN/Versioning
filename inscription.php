9299fe518bc1efbc94e83937d1acb7a83ec87b88
Nau-LAN <nclavereuil@gmail.com>
2022-09-28 14:52:10 +0200

    ajout inscription et verification

---
+++ inscription.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="uft-8">
+        <title>Inscription</title>
+    </head>
+
+    <body>
+        <div>
+            WIP
+        </div>
+
+
+    </body>
+</html>
---
+++ verification.php
@@ -0,0 +1,4 @@
+<?php
+
+$user = $_POST['username'];
+$mdp = $_POST ['password'];
